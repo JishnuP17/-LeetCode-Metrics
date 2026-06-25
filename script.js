b5faaad9f8f4d24469b4a8aac6c560c5824843cb
JishnuP17 <padipurakkal.jishnu@gmail.com>
2026-06-25 08:12:20 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function() {
+
+
     const searchButton = document.getElementById("search-btn");
     const usernameInput = document.getElementById("user-input");
     const statsContainer = document.querySelector(".stats-container");
@@ -10,18 +12,20 @@ document.addEventListener("DOMContentLoaded",function(){
     const hardLabel = document.getElementById("hard-label");
     const cardStatsContainer = document.querySelector(".stats-cards");
 
-    function validateUsername(username){
-        if(username.trim()===""){
-            alert("Username is Empty");
+    //return true or false based on a regex
+    function validateUsername(username) {
+        if(username.trim() === "") {
+            alert("Username should not be empty");
             return false;
         }
-        const regex = /^[a-zA-Z0-9_-]{1,30}$/;
+        const regex = /^[a-zA-Z0-9_-]{1,15}$/;
         const isMatching = regex.test(username);
-        if(!isMatching){
-            alert("invalid Username");
+        if(!isMatching) {
+            alert("Invalid Username");
         }
         return isMatching;
     }
+
     async function fetchUserDetails(username) {
 
         try{
@@ -63,29 +67,54 @@ document.addEventListener("DOMContentLoaded",function(){
             searchButton.disabled = false;
         }
     }
+
     function updateProgress(solved, total, label, circle) {
         const progressDegree = (solved/total)*100;
         circle.style.setProperty("--progress-degree", `${progressDegree}%`);
         label.textContent = `${solved}/${total}`;
     }
-    function displayUserData(parsedData){
+
+
+    function displayUserData(parsedData) {
         const totalQues = parsedData.data.allQuestionsCount[0].count;
         const totalEasyQues = parsedData.data.allQuestionsCount[1].count;
         const totalMediumQues = parsedData.data.allQuestionsCount[2].count;
         const totalHardQues = parsedData.data.allQuestionsCount[3].count;
+
         const solvedTotalQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
         const solvedTotalEasyQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
         const solvedTotalMediumQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
         const solvedTotalHardQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
+
         updateProgress(solvedTotalEasyQues, totalEasyQues, easyLabel, easyProgressCircle);
         updateProgress(solvedTotalMediumQues, totalMediumQues, mediumLabel, mediumProgressCircle);
         updateProgress(solvedTotalHardQues, totalHardQues, hardLabel, hardProgressCircle);
+
+        const cardsData = [
+            {label: "Overall Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[0].submissions },
+            {label: "Overall Easy Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[1].submissions },
+            {label: "Overall Medium Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[2].submissions },
+            {label: "Overall Hard Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[3].submissions },
+        ];
+
+        console.log("card ka data: " , cardsData);
+
+        cardStatsContainer.innerHTML = cardsData.map(
+            data => 
+                    `<div class="card">
+                    <h4>${data.label}</h4>
+                    <p>${data.value}</p>
+                    </div>`
+        ).join("")
+
     }
-    searchButton.addEventListener('click',function(){
+
+    searchButton.addEventListener('click', function() {
         const username = usernameInput.value;
-        if(validateUsername(username)){
-            console.log(`Username ${username}`);
+        console.log("logggin username: ", username);
+        if(validateUsername(username)) {
             fetchUserDetails(username);
-        }  
+        }
     })
+
 })
